19391c7bc043b2f850214cf355f4b127b6cdc37d
prithvi5481 <prithvisingh91827@gmail.com>
2025-04-22 22:25:41 +0530

    Add capacitor setup

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'shoppin',
+  webDir: 'build'
+};
+
+export default config;
